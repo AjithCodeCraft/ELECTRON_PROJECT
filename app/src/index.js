6287c43f3d31090be3c232170986b45e60ec8f79
Your Name <ajithsa900@gmail.com>
2024-12-30 15:40:19 +0530

    adding admin

--- app/src/index.js
+++ app/src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
+import Admin from './pages/admin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-        </Route>
+        <Route path="/" element={<Layout />}> </Route>
+        <Route path='/admin' element ={<Admin />}></Route>
+
+      
+       
       </Routes>
     </HashRouter>
   </React.StrictMode>
