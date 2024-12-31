7b76d9ad3a973c7f8d54ff319e49ac14d4b5f0f2
Your Name <ajithsa900@gmail.com>
2024-12-31 20:31:46 +0530

    routing fixed

--- app/src/index.js
+++ app/src/index.js
@@ -6,29 +6,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 
 import { Admin } from './pages/admin';
-import { User } from './pages/user'
-import { Login }from './pages/login';
+import { User } from './pages/user';
+import { Login } from './pages/login';
 import { Signup } from './pages/signup';
 
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-
-        {/* Main layout */}
-        <Route path="/" element={<Layout />}>
-          {/* Admin page as a nested route */}
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/user" element={<User />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={< Signup/>} />
-          
-        </Route>
-
+        {/* Define root routes */}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/layout" element={<Layout />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
